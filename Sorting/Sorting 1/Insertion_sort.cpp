ad3fe06025a69fabdf3be705d8b61fbdeb04adb1
tejashvi-t <tejashvi2812@gmail.com>
2023-08-28 18:38:43 +0530

    Insertion sort

---
+++ Sorting/Sorting 1/Insertion_sort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// insertion sort
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > current)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = current;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+}
